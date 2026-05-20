3cb4c0c2c6dfa10d1177ed7c85421329b6cbed6c
Sze Chun <scyiu@cn033.int>
2026-05-20 10:24:01 +0200

    mascot: add lumi companion expressions

--- components/mascot/index.ts
+++ components/mascot/index.ts
@@ -1,2 +1,4 @@
 export { DalaMascot } from './DalaMascot';
+export { LumiMascot } from './LumiMascot';
 export type { DalaMascotProps, MascotExpression } from './DalaMascot';
+export type { LumiMascotProps } from './LumiMascot';
